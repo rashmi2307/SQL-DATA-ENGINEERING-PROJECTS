72a3d0876c18d7feb0ee1946ee1fa9806a07e619
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-06-02 01:57:58 +0530

    Updated CTE

--- Lessons/1.23_Subquery_CTE.sql
+++ Lessons/1.23_Subquery_CTE.sql
@@ -21,6 +21,8 @@ WITH valid_salaries AS (
 SELECT * FROM valid_salaries
 LiMIT 10;
 
+-- Subquery Examples
+
 -- Scenario 1 - Subquery in "SELECT"
 -- Show each job's salary next to the overall market median
 
@@ -87,3 +89,79 @@ HAVING MEDIAn(salary_year_avg) > (
 )
 LIMIT 10;
 
+
+-- CTE Example
+-- Compare how much more (or less) remote roles pay compared to onsite roles for each job title.
+-- Use a CTE to calculate the median salary by title and work arrangement, then compare those medians.
+
+
+WITH title_median AS (
+    SELECT 
+        job_title_short,
+        job_work_from_home,
+        CAST(MEDIAN(salary_year_avg) AS INT) AS median_salary
+    FROM data_jobs.job_postings_fact 
+    WHERE job_country = 'United States'
+    GROUP BY    
+        job_title_short,
+        job_work_from_home
+)
+SELECT 
+    r.job_title_short,
+    r.median_salary AS remote_median_salary,
+    o.median_salary AS onsite_median_salary,
+    (r.median_salary - o.median_salary) AS remote_premium
+FROM title_median AS r
+INNER JOIN title_median AS o 
+    ON r.job_title_short  = o.job_title_short
+WHERE r.job_work_from_home = TRUE 
+    AND o.job_work_from_home = FALSE
+ORDER BY remote_premium DESC;
+
+
+
+SELECT *
+FROM range(3) AS src(key);
+
+SELECT *
+FROM range(2) AS tgt(key);
+
+SELECT *
+FROM range(3) AS src(key)
+WHERE EXISTS (
+    SELECT 1
+    FROM range(2) AS tgt(key)
+    WHERE tgt.key = src.key
+);
+
+SELECT *
+FROM range(3) AS src(key)
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM range(2) AS tgt(key)
+    WHERE tgt.key = src.key
+);
+
+
+-- Final Example
+-- Identify job postings that have no associated skills before loading them into a data mart
+
+SELECT *
+FROM data_jobs.job_postings_fact
+ORDER BY job_id
+LIMIT 10;
+
+SELECT *
+FROM data_jobs.skills_job_dim
+ORDER BY job_id
+LIMIT 40;
+
+
+SELECT *
+FROM data_jobs.job_postings_fact AS tgt
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM data_jobs.skills_job_dim AS src
+    WHERE tgt.job_id = src.job_id
+)
+ORDER BY job_id;
