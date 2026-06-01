797590e9ff9b1eb588b73484285c39c6cf8011d2
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-06-02 03:46:19 +0530

    DDL and DML Pt3 MERGE Operation

---
+++ Lessons/1.24_DDL_DML_Pt3/priority_roles.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE TABLE staging.priority_roles (
+    role_id INTEGER PRIMARY KEY,
+    role_name VARCHAR,
+    priority_lvl INTEGER
+);
+
+INSERT INTO staging.priority_roles (role_id,role_name,priority_lvl)
+VALUES 
+    (1,'Data Engineer',2),
+    (2,'Senior Data Engineer',1),
+    (3,'Data Analyst',3);
+
+SELECT * FROM staging.priority_roles;
