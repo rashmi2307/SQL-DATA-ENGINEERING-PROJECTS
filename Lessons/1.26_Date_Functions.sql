b0001ef0962ad2bf2f4d5c5aeacb0c79bd89008d
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-06-02 22:16:04 +0530

    Date Functions EXTRACT

--- Lessons/1.26_Date_Functions.sql
+++ Lessons/1.26_Date_Functions.sql
@@ -6,3 +6,27 @@ SELECT
     CAST(job_posted_date AS TIMESTAMPTZ) AS timestamptz
 FROM job_postings_fact
 LIMIT 10;
+
+
+SELECT 
+    job_posted_date,
+    EXTRACT (YEAR FROM job_posted_date) AS job_posted_year,
+    EXTRACT (MONTH FROM job_posted_date) AS job_posted_month,
+    EXTRACT (DAY FROM job_posted_date) AS job_posted_day
+FROM job_postings_fact
+LIMIT 10;
+
+
+SELECT 
+    EXTRACT (YEAR FROM job_posted_date) AS job_posted_year,
+    EXTRACT (MONTH FROM job_posted_date) AS job_posted_month,
+    COUNT(job_id) AS job_count
+FROM job_postings_fact
+WHERE job_title_short = 'Data Engineer'
+GROUP BY 
+    EXTRACT (YEAR FROM job_posted_date),
+    EXTRACT (MONTH FROM job_posted_date)
+ORDER BY 
+    job_posted_year,
+    job_posted_month;
+
