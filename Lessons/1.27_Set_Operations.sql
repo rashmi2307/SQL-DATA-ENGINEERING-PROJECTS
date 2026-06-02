f6a7bd914a0dd8b601fae62e8760ae9ad5903d27
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-06-03 02:16:10 +0530

    Set Operations

---
+++ Lessons/1.27_Set_Operations.sql
@@ -0,0 +1,96 @@
+SELECT UNNEST([1,1,1,2])
+UNION
+SELECT UNNEST([1,1,3]);
+
+SELECT UNNEST([1,1,1,2])
+UNION ALL
+SELECT UNNEST([1,1,3]);
+
+
+SELECT UNNEST([1,1,1,2])
+INTERSECT
+SELECT UNNEST([1,1,3]);
+
+
+SELECT UNNEST([1,1,1,2])
+INTERSECT ALL
+SELECT UNNEST([1,1,3]);
+
+
+SELECT UNNEST([1,1,1,2])
+EXCEPT 
+SELECT UNNEST([1,1,3]);
+
+
+SELECT UNNEST([1,1,1,2])
+EXCEPT ALL
+SELECT UNNEST([1,1,3]);
+
+
+-- Which unique job postings appeared in either 2023 or 2024?
+-- Which job postings appeared across both years, counting duplicates?
+-- Which job postings appeared in both 2023 and 2024?
+-- Which job postings appeared in both years, preserving duplicate counts?
+-- Which job postings appeared in 2023 but not in 2024?
+-- Which job postings appeared more times in 2023 than 2024, one-for-one?
+
+CREATE TEMP TABLE jobs_2023 AS
+SELECT * EXCLUDE (job_id,job_posted_date)      -- EXCLUDE supported by only DuckDb, not other platforms
+FROM job_postings_fact
+WHERE EXTRACT(YEAR FROM job_posted_date) = 2023;
+
+SELECT * FROM jobs_2023;
+
+CREATE TEMP TABLE jobs_2024 AS
+SELECT * EXCLUDE (job_id,job_posted_date)      -- EXCLUDE supported by only DuckDb, not other platforms
+FROM job_postings_fact
+WHERE EXTRACT(YEAR FROM job_posted_date) = 2024;
+
+SELECT * FROM jobs_2024;
+
+-- Which unique job postings appeared in either 2023 or 2024?
+
+SELECT * FROM jobs_2023 
+UNION 
+SELECT * FROM jobs_2024;
+
+SELECT 
+    'jobs_2023' AS table_name,
+    COUNT(*) AS record_count
+FROM jobs_2023 
+UNION 
+SELECT 
+    'jobs_2024' AS table_name,
+    COUNT(*)
+FROM jobs_2024;
+
+-- Which job postings appeared across both years, counting duplicates?
+
+SELECT * FROM jobs_2023 
+UNION  ALL
+SELECT * FROM jobs_2024;
+
+-- Which job postings appeared in both 2023 and 2024?
+
+SELECT * FROM jobs_2023 
+INTERSECT
+SELECT * FROM jobs_2024;
+
+-- Which job postings appeared in both years, preserving duplicate counts?
+
+SELECT * FROM jobs_2023 
+INTERSECT ALL
+SELECT * FROM jobs_2024;
+
+-- Which job postings appeared in 2023 but not in 2024?
+
+SELECT * FROM jobs_2023 
+EXCEPT 
+SELECT * FROM jobs_2024;
+
+-- Which job postings appeared more times in 2023 than 2024, one-for-one?
+
+SELECT * FROM jobs_2023 
+EXCEPT ALL
+SELECT * FROM jobs_2024;
+
