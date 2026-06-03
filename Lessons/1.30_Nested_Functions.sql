30866ef88af5e72481b6c58275d20edf0b506160
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-06-04 00:00:21 +0530

    Nested Functions

--- Lessons/1.30_Nested_Functions.sql
+++ Lessons/1.30_Nested_Functions.sql
@@ -124,7 +124,9 @@ SELECT
 FROM skill_map;
 
 
- -- JSON 
+
+
+-- JSON 
 
 
 WITH raw_skill_json AS (
@@ -137,8 +139,6 @@ SELECT
         type:= json_extract_string(skill_json,'$.type')
     )
 FROM raw_skill_json;
-
-
 
 
 WITH raw_json AS (
@@ -163,18 +163,97 @@ FROM raw_json,json_each(skills_json) AS e;
 
 
 
+-- Arrays Final Example
+-- Build a flat skill table for co--workers to access job titles, salary info, and skills in one table
+
+CREATE OR REPLACE TEMP TABLE job_skills_array AS
+SELECT 
+    jpf.job_id,
+    jpf.job_title_short,
+    jpf.salary_year_avg,
+    ARRAY_AGG(sd.skills) AS skills_array
+FROM job_postings_fact AS jpf 
+LEFT JOIN skills_job_dim AS sjd 
+    ON jpf.job_id = sjd.job_id
+LEFT JOIN skills_dim AS sd 
+    ON sd.skill_id = sjd.skill_id
+GROUP BY ALL;
 
 
 
 
+-- From the prespective of a Data Analyst, analyze the median salary per skill
 
 
+WITH flat_skills AS (
+    SELECT  
+        job_id,
+        job_title_short,
+        salary_year_avg,
+        UNNEST(skills_array) AS skill
+    FROM job_skills_array
+)
+SELECT 
+    skill,
+    MEDIAN(salary_year_avg) AS median_salary
+FROM flat_skills
+GROUP BY skill
+ORDER BY median_salary DESC
+LIMIT 20;
 
 
+-- Array of Structs Final Example
 
 
+CREATE OR REPLACE TEMP TABLE job_skills_array_struct AS
+SELECT 
+    jpf.job_id,
+    jpf.job_title_short,
+    jpf.salary_year_avg,
+    ARRAY_AGG(
+        STRUCT_PACK(
+            skill_type:= sd.type,
+            skill_name:= sd.skills
+        )
+    ) AS skills_type
+FROM job_postings_fact AS jpf 
+LEFT JOIN skills_job_dim AS sjd 
+    ON jpf.job_id = sjd.job_id
+LEFT JOIN skills_dim AS sd 
+    ON sd.skill_id = sjd.skill_id
+GROUP BY ALL;
 
 
+-- From the prespective of a Data Analyst, analyze the median salary per skill
+
+
+SELECT  
+    job_id,
+    job_title_short,
+    salary_year_avg,
+    UNNEST(skills_type).skill_type AS skill_type,
+    UNNEST(skills_type).skill_name AS skill_name
+FROM
+    job_skills_array_struct;
+
+
+WITH flat_skills AS (
+    SELECT  
+        job_id,
+        job_title_short,
+        salary_year_avg,
+        UNNEST(skills_type).skill_type AS skill_type,
+        UNNEST(skills_type).skill_name AS skill_name
+    FROM
+        job_skills_array_struct
+)
+SELECT 
+    skill_type,
+    MEDIAN(salary_year_avg) AS median_salary
+FROM flat_skills
+GROUP BY skill_type
+ORDER BY median_salary DESC
+LIMIT 20;
 
 
 
