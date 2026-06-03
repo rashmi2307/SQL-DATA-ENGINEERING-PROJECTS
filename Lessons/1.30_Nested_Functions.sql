8e10fcba2e52243eb50c7bc261d7c508d7c28e3d
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-06-03 22:27:29 +0530

    Struct done

--- Lessons/1.30_Nested_Functions.sql
+++ Lessons/1.30_Nested_Functions.sql
@@ -44,16 +44,35 @@ FROM skills_array;
 
 -- STRUCT Intro
 
+SELECT {skill: 'python', type: 'programming'} AS skills_struct;
 
 
+WITH skill_struct AS(
+    SELECT 
+        STRUCT_PACK(
+            skill:= 'python',
+            type:= 'programming'
+        ) AS s
+)
+SELECT 
+    s.skill,
+    s.type
+FROM skill_struct;
 
 
-
-
-
-
-
-
+WITH skill_table AS (
+    SELECT 'python' AS skills, 'programming' AS types
+    UNION ALL
+    SELECT 'sql', 'query_language'
+    UNION ALL
+    SELECT 'r', 'programming'
+)
+SELECT
+    STRUCT_PACK(
+        skill:=skills,
+        type:=types
+    )
+FROM skill_table;
 
 
 
