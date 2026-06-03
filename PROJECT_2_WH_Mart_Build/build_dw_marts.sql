adaa61cfe1d069e2c24654a19f509e2b140fbc7d
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-06-04 03:40:03 +0530

    created flat mart and updated build mart script

--- PROJECT_2_WH_Mart_Build/build_dw_marts.sql
+++ PROJECT_2_WH_Mart_Build/build_dw_marts.sql
@@ -22,8 +22,8 @@
 -- Step 2: DW - Load data from CSV files into star schema
 .read 02_load_schema_dw.sql
 
--- -- Step 3: Mart - Create flat mart (denormalized table)
--- .read 03_create_flat_mart.sql
+-- Step 3: Mart - Create flat mart (denormalized table)
+.read 03_create_flat_mart.sql
 
 -- -- Step 4: Mart - Create skills demand mart
 -- .read 04_create_skills_mart.sql
